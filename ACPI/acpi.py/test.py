3f864af66884ec0c64491aa0664cf29f98d0097e
sorgue <sorgue>
2003-07-22 20:29:10 +0000

    Added an example file, to test acpi.py

---
+++ ACPI/acpi.py/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import acpi
+from acpi import *
+
+myAcpi = Acpi()
+myAcpi.update()
+
+
+print "fan : " + str(myAcpi.fan_state())
+print "ac-adapter : " + str(myAcpi.charging_state())
+print "nb batteries : " + str(myAcpi.nb_of_batteries())
+print "charge: " + str(myAcpi.capacity())
+print "percent: " + str(myAcpi.percent())
+print "lifetime: " + myAcpi.estimated_lifetime()
+print "temperature: "+ myAcpi.temperature()
+print "Frequency: "+ myAcpi.frequency()
+print "Available freqencies: "+str(myAcpi.performance_states())
+
+print "Change to "+myAcpi.performance_states()[1]
+print "result: "+ str(myAcpi.setFrequency(myAcpi.performance_states()[1]))
+
