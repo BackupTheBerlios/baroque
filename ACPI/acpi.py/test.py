e32301d48abd54dc7a1b86be0407b17814ca4089
riemer <riemer>
2003-08-25 12:10:14 +0000

    adaptions for changed interface

--- ACPI/acpi.py/test.py
+++ ACPI/acpi.py/test.py
@@ -7,16 +7,16 @@ myAcpi = Acpi()
 myAcpi.update()
 
 
-print "fan : " + str(myAcpi.fan_state())
+print "fan : " + str(myAcpi.fan_state("FAN0"))
 print "ac-adapter : " + str(myAcpi.charging_state())
 print "nb batteries : " + str(myAcpi.nb_of_batteries())
 print "charge: " + str(myAcpi.capacity())
 print "percent: " + str(myAcpi.percent())
 print "lifetime: " + myAcpi.estimated_lifetime()
-print "temperature: "+ str(myAcpi.temperature())
-print "Frequency: "+ myAcpi.frequency()
-print "Available freqencies: "+str(myAcpi.performance_states())
+print "temperature: "+ str(myAcpi.temperature("THRM"))
+#print "Frequency: "+ myAcpi.frequency(0)
+#print "Available freqencies: "+str(myAcpi.performance_states(0))
 
-print "Change to "+myAcpi.performance_states()[1]
-print "result: "+ str(myAcpi.set_frequency(myAcpi.performance_states()[1]))
+#print "Change to "+myAcpi.performance_states()[1]
+#print "result: "+ str(myAcpi.set_frequency(myAcpi.performance_states()[1]))
 
